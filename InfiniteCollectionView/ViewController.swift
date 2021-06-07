4eadf84abc22612ee76b60cd82fb7ede88d5d415
Ed <Siniakov>
2021-06-07 19:39:51 +0300

    Initial Commit

---
+++ InfiniteCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfiniteCollectionView
+//
+//  Created by Eduard Sinyakov on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

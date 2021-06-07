82c7e74ed8da821f1553b4fc9cfc5bebef2c6c3d
Ed <Siniakov>
2021-06-07 20:44:51 +0300

    in progress

--- InfiniteCollectionView/ViewController.swift
+++ InfiniteCollectionView/ViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let items = [UIColor.red, UIColor.magenta, UIColor.green]
+    var items = [UIColor.red, UIColor.magenta, UIColor.green]
+//    var images = [UIImage(named: "0"), UIImage(named: "1"), UIImage(named: "2")]
+    var images = [UIImage(named: "0")!, UIImage(named: "1")!, UIImage(named: "2")!]
+    var isInitial = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.contentInsetAdjustmentBehavior = .never
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//            self.items += self.items
+//            self.images += self.images
+//            self.collectionView.reloadData()
+            self.isInitial = false
+        }
     }
 
 
@@ -27,18 +38,62 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let itemToShow = items[indexPath.row % items.count]
+//        let itemToShow = items[indexPath.row]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CVCell", for: indexPath) as! CVCell
-        cell.backgroundColor = items[indexPath.row]
+        cell.update(image: images[indexPath.row])
+        cell.backgroundColor = itemToShow
+        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        CGSize(width: 200, height: 115)
+        CGSize(width: 200, height: 110)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+//        guard !isInitial else { return }
+//        print(indexPath.row)
+        print(indexPath.row)
+//        if indexPath.row == items.count - 1 {
+//            items += [UIColor.red, UIColor.magenta, UIColor.green]
+//            images += [UIImage(named: "0")!, UIImage(named: "1")!, UIImage(named: "2")!]
+//            self.collectionView.insertItems(at: [IndexPath(row: items.count - 1, section: 0), IndexPath(row: items.count - 2, section: 0), IndexPath(row: items.count - 3, section: 0)])
+//        }
+//        guard !isInitial else {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//                self.isInitial = false
+//            }
+//
+//            return
+//        }
+//        print(indexPath.row)
+//        if indexPath.row == items.count - 1 {
+//
+//            items += items
+//            images += images
+//            collectionView.reloadData()
+//            print(indexPath.row, "reloadData", items.count, items)
+//        }
+    }
+    
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        print(scrollView.contentOffset.x, scrollView.contentSize.width, scrollView.contentInset.right)
+//        if scrollView.bounds.width == scrollView.contentSize.width {
+//            print("here")
+//        }
+//    }
+    
+    
     
 }
 
 
 class CVCell: UICollectionViewCell {
+    @IBOutlet weak var avatar: UIImageView!
     
+    func update(image: UIImage) {
+        avatar.clipsToBounds = true
+        avatar.image = image
+    }
 }
